3c11698435c141b31f665ebd573c77802da00643
Nate Vogel <nathan.c.vogel@gmail.com>
2021-01-04 21:28:04 -0500

    fixed typo

--- ujs_search/urls.py
+++ ujs_search/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path("search/name/", SearchName.as_view()),
     path("search/docket/", SearchDocket.as_view()),
-    path("search/docket/many/", SearchDockets.as_view()),
+    path("search/docket/many/", SearchMultipleDockets.as_view()),
 ]
