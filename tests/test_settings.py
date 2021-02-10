aca3ef2d0c22bcd28d836b3b63a9d4d86a003e12
Nate Vogel <nathan.c.vogel@gmail.com>
2021-02-10 10:35:54 -0500

    added a django site to this plugin. now i can run the plugin directly, and I think I can also just retire the cleanslate screener and eg-docketsearch that really just used this same thing without adding anything to it.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,7 +1,7 @@
-SECRET_KEY = 'fake-key'
-DEBUG=True
+SECRET_KEY = "fake-key"
+DEBUG = True
 REST_FRAMEWORK = {
-    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
 
 INSTALLED_APPS = [
@@ -9,5 +9,4 @@ INSTALLED_APPS = [
     "ujs_search",
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
-
+EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
